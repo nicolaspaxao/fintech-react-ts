39bba0649e8ebd22685f558978af52ba176dade5
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-07-15 15:51:59 -0300

    Finalizando a sidenav

--- src/Components/Sidednav.tsx
+++ src/Components/Sidednav.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import FintechSVG from '../assets/FintechSVG';
+
+import resumo from '../assets/icons/resumo.svg';
+import vendas from '../assets/icons/vendas.svg';
+import webhooks from '../assets/icons/webhooks.svg';
+import configuracoes from '../assets/icons/configuracoes.svg';
+import contato from '../assets/icons/contato.svg';
+import sair from '../assets/icons/sair.svg';
 
 export const Sidednav = () => {
   return (
-    <div>Sidednav</div>
+    <nav className='sidenav box bg-3'>
+      <FintechSVG title={'Fintech'} />
+      <ul>
+        <li>
+          <span><img src={resumo} /></span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span><img src={vendas} /></span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span><img src={webhooks} /></span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span><img src={configuracoes} /></span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span><img src={contato} /></span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span><img src={sair} /></span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
